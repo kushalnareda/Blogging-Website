b06261c6d77a3be7a8a1819703ee4f1eba2f0d33
kushalnareda <bb1211214@iitd.ac.in>
2024-05-25 15:08:18 +0530

    New components
    Blog + App bar

---
+++ frontend/src/components/AppBar.tsx
@@ -0,0 +1,14 @@
+import { Avatar } from "./BlogCard"
+
+export const Appbar = () => {
+    return (
+        <div className="border-b flex justify-between px-10 py-4">
+            <div className="flex justify-center flex-col">
+                Medium
+            </div>
+            <div>
+                <Avatar name="kushal" size={"big"} /> 
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,7 +1,28 @@
-export const Blogs= ()=>{
-    return (
-        <div>
-            Blogs
+import { Appbar } from "../components/AppBar";
+import { BlogCard } from "../components/BlogCard";
+import { useBlogs } from "../hooks";
+
+export const Blogs = () => {
+  const { loading, blogs } = useBlogs();
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  return (
+    <div>
+      <Appbar />
+      <div className="flex justify-center">
+        <div className=" max-w-xl">
+          {blogs.map((blog) => (
+            <BlogCard
+              id={1}
+              authorName= {blog.author.name || "User"}
+              title={blog.title}
+              content={blog.content}
+              publishedDate="2024-05-25"
+            />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
